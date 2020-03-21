470707a91b1ea9a00b7e8595885fa0336be93099
Illia Lytvynov <illial@wix.com>
2020-03-21 12:26:02 +0200

    Added functions

--- practice_7/functions.js
+++ practice_7/functions.js
@@ -3,19 +3,54 @@ export function toString(stringArray) {
   for (let i = 0; i < stringArray.length; i++) {
     resultStr += '-' + stringArray[i];
   }
-
   return resultStr;
 }
 
 export function totalSum(numberArray) {
-  //
-  return;
+  let total = 0;
+  for (let i = 0; i < numberArray.length; i++) {
+    if (typeof numberArray[i] !== 'number') {
+      continue;
+    }
+    total += numberArray[i];
+  }
+  return total;
 }
 
 export function mapArray(stringArray) {
-  //
-  return [];
+  const numberArray = [];
+  for (let item of stringArray) {
+    numberArray.push(item.length); // вернет колличество символов в строке
+  }
+  return numberArray;
 }
 
-// const newArray = mapArray(['hi', 'test', 'ttt']);
-// newArray = [2, 4, 3]
+export function findMax(numberArray) {
+  let maxValue = numberArray[0];
+  for (let i = 1; i < numberArray.length; i++) {
+    if (numberArray[i] > maxValue) {
+      maxValue = numberArray[i];
+    }
+  }
+  return maxValue;
+}
+
+export function getTotalPrice(arrayProducts) {
+  let total = 0;
+  for (let i = 0; i < arrayProducts.length; i++) {
+    total += arrayProducts[i].price;
+  }
+  return total;
+}
+
+export function filterMaxValues(arrayToFilter, maxValue) {
+  const filteredValues = [];
+  for (let someValue of arrayToFilter) {
+    console.log(someValue);
+    if (someValue < maxValue) {
+      filteredValues.push(someValue);
+    }
+  }
+
+  return filteredValues;
+}
--- practice_7/index.js
+++ practice_7/index.js
@@ -1,6 +1,38 @@
-import { toString } from './functions.js';
+import {
+  toString,
+  totalSum,
+  mapArray,
+  findMax,
+  getTotalPrice,
+  filterMaxValues
+} from './functions.js';
 
 const str = toString(['Hello', 'world']);
 console.log(str);
-const cartTotal = totalSum([10, 20, 50, 100]);
+const cartTotal = totalSum([10, '20', 50, 100]);
 console.log(cartTotal);
+const strLength = ['Hello', 'worldфывфыв'].map(item => {
+  return item.length;
+});
+console.log(strLength);
+const max = findMax([10, 20, 10, 5, 26, 100]);
+console.log(max);
+
+const cart = [
+  {
+    name: 'Iphone',
+    price: 1400
+  },
+  {
+    name: 'MacBook',
+    price: 3000
+  },
+  {
+    name: 'Samsung galaxy',
+    price: 1200
+  }
+];
+
+const totalPrice = getTotalPrice(cart);
+const filteredValues = filterMaxValues([0, 23, 45, 15, 48, 20, 24, 55], 40);
+console.log('FILTERD VALUES', filteredValues);
