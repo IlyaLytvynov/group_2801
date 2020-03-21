34b716f3582ab4190e1ecd49241b3c1f9918f3eb
Illia Lytvynov <illial@wix.com>
2020-03-21 11:22:36 +0200

    addedd functions

---
+++ practice_7/functions.js
@@ -0,0 +1,21 @@
+export function toString(stringArray) {
+  let resultStr = '';
+  for (let i = 0; i < stringArray.length; i++) {
+    resultStr += '-' + stringArray[i];
+  }
+
+  return resultStr;
+}
+
+export function totalSum(numberArray) {
+  //
+  return;
+}
+
+export function mapArray(stringArray) {
+  //
+  return [];
+}
+
+// const newArray = mapArray(['hi', 'test', 'ttt']);
+// newArray = [2, 4, 3]
---
+++ practice_7/index.js
@@ -0,0 +1,6 @@
+import { toString } from './functions.js';
+
+const str = toString(['Hello', 'world']);
+console.log(str);
+const cartTotal = totalSum([10, 20, 50, 100]);
+console.log(cartTotal);
